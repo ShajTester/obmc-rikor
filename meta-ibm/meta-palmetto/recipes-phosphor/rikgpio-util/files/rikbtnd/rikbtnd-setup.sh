8d97750d0777530796f06690b53fcc0903272aa2
ShajTester <kolobrod2002@yandex.ru>
2019-10-30 13:55:37 +0300

    Мелкие исправления
    Инициализация вентиляторов в 150 всех.
    Отмена изменений в skeleton.

--- meta-ibm/meta-palmetto/recipes-phosphor/rikgpio-util/files/rikbtnd/rikbtnd-setup.sh
+++ meta-ibm/meta-palmetto/recipes-phosphor/rikgpio-util/files/rikbtnd/rikbtnd-setup.sh
@@ -2,14 +2,14 @@
 
 #For 2U servers
 echo "Initializing PWM value ..."
-echo 50 > /sys/bus/platform/devices/1e786000.pwm-tacho-controller/hwmon/hwmon0/pwm1	#0
-echo 50 > /sys/bus/platform/devices/1e786000.pwm-tacho-controller/hwmon/hwmon0/pwm2	#1
-echo 50 > /sys/bus/platform/devices/1e786000.pwm-tacho-controller/hwmon/hwmon0/pwm4	#3
-echo 50 > /sys/bus/platform/devices/1e786000.pwm-tacho-controller/hwmon/hwmon0/pwm3	#2
-echo 50 > /sys/bus/platform/devices/1e786000.pwm-tacho-controller/hwmon/hwmon0/pwm5	#4
-echo 50 > /sys/bus/platform/devices/1e786000.pwm-tacho-controller/hwmon/hwmon0/pwm6	#5
-echo 120 > /sys/bus/platform/devices/1e786000.pwm-tacho-controller/hwmon/hwmon0/pwm7	#6	#CPU0
-echo 120 > /sys/bus/platform/devices/1e786000.pwm-tacho-controller/hwmon/hwmon0/pwm8	#7	#CPU1
+echo 150 > /sys/bus/platform/devices/1e786000.pwm-tacho-controller/hwmon/hwmon0/pwm1	#0
+echo 150 > /sys/bus/platform/devices/1e786000.pwm-tacho-controller/hwmon/hwmon0/pwm2	#1
+echo 150 > /sys/bus/platform/devices/1e786000.pwm-tacho-controller/hwmon/hwmon0/pwm4	#3
+echo 150 > /sys/bus/platform/devices/1e786000.pwm-tacho-controller/hwmon/hwmon0/pwm3	#2
+echo 150 > /sys/bus/platform/devices/1e786000.pwm-tacho-controller/hwmon/hwmon0/pwm5	#4
+echo 150 > /sys/bus/platform/devices/1e786000.pwm-tacho-controller/hwmon/hwmon0/pwm6	#5
+echo 150 > /sys/bus/platform/devices/1e786000.pwm-tacho-controller/hwmon/hwmon0/pwm7	#6	#CPU0
+echo 150 > /sys/bus/platform/devices/1e786000.pwm-tacho-controller/hwmon/hwmon0/pwm8	#7	#CPU1
 
 #For 1U servers
 #echo "Initializing PWM value ..."
